9fb38e833c8b9bf8cce2d834d5ab00697bd7161c
beamozan000 <1074692002z>
2021-11-12 12:00:51 -0500

    U3 L1 Notes Completed

--- L1-If Statements/src/AddORMultiply.java
+++ L1-If Statements/src/AddORMultiply.java
@@ -1,4 +1,35 @@
+import javax.accessibility.Accessible;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.Scanner;
+
+
 public class AddORMultiply {
+
+    public static void main(String[] args) {
+        double numImput1 = Double.parseDouble(JOptionPane.showInputDialog("first number"));
+        double numImput2 = Double.parseDouble(JOptionPane.showInputDialog("second number"));
+        double result;
+
+        if(numImput1 > numImput2){
+           result = multiply(numImput1, numImput2);
+        }
+        if(numImput1 <= numImput2){
+            result = add(numImput1, numImput2);
+        }
+
+        JOptionPane.showMessageDialog(null, "the result is:" + result);
+    }
+
+    public static double add(double num1, double num2){
+
+        return num1 + num2;
+    }
+    public static double multiply(double num1, double num2){
+        return num1 * num2;
+    }
+
+
 
     /*
     Write two methods, one that adds two numbers and second that multiplies them.
